ac4a21f7591d7c7ab192b865e1c01317bc27b64c
Yash Rai <yrai5303@gmail.com>
2023-09-13 15:09:56 +0530

    The web app is ready

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import pickle
-from sklearn.linear_model import LinearRegression
 from flask import Flask, request, jsonify, render_template
 from sklearn.preprocessing import StandardScaler
 import numpy as np
@@ -10,8 +9,7 @@ app = Flask(__name__)
 model = pickle.load(open("regmodel.pkl", "rb"))
 
 # Define and load the scaler
-with open("scaling.pkl", "rb") as scaler_file:
-    scaler = pickle.load(scaler_file)
+scaler = pickle.load(open("scaling.pkl", "rb"))
 
 @app.route('/')
 def home():
@@ -19,13 +17,19 @@ def home():
 
 @app.route("/predict_api", methods=["POST"])
 def predict_api():
-    data = request.json['data']
+    data = request.json
     print(data)
-    print(np.array(list(data.values())).reshape(1, -1))
-    new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
+    input_data = np.array(list(data.values())).reshape(1, -1)
+    new_data = scaler.transform(input_data)
     output = model.predict(new_data)
-    print(output)
-    return jsonify(output[0])
+    return jsonify({"prediction": output[0]})
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
+    output = model.predict(final_input)[0]
+    return render_template("home.html", prediction_text="The predicted house price is {:.2f}".format(output)) 
 
 if __name__ == "__main__":
     app.run(debug=True)
