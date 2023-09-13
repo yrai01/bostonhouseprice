29f8981d6ffa15fcbd34644507aaaaafe0fbc981
Yash Rai <yrai5303@gmail.com>
2023-09-13 12:23:35 +0530

    This is commit for my web application

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
 import pickle
-from flask import Flask,request,app,jsonify,url_for,render_template,redirect,flash,session,escape
+from sklearn.linear_model import LinearRegression
+from flask import Flask, request, jsonify, render_template
+from sklearn.preprocessing import StandardScaler
 import numpy as np
-import pandas as pd
 
-app=Flask(__name__)
-#load the model
-model=pickle.load(open("regmodel.pkl","rb"))
+app = Flask(__name__)
+
+# Load the model
+model = pickle.load(open("regmodel.pkl", "rb"))
+
+# Define and load the scaler
+with open("scaling.pkl", "rb") as scaler_file:
+    scaler = pickle.load(scaler_file)
+
 @app.route('/')
 def home():
     return render_template("home.html")
 
-@app.route("/predict_api",methods=["POST"])
-
+@app.route("/predict_api", methods=["POST"])
 def predict_api():
     data = request.json['data']
     print(data)
-    
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
+    output = model.predict(new_data)
+    print(output)
+    return jsonify(output[0])
+
+if __name__ == "__main__":
+    app.run(debug=True)
